59b63c80ee497a0c8a9f0333f33b667a679c1740
SeungWook0502 <SeungWook0502@gmail.com>
2021-03-11 13:57:49 +0900

    remove \n & Crawling of Article 6 hours ago

--- src/Today_News/MainApp.java
+++ src/Today_News/MainApp.java
@@ -1,5 +1,8 @@
 package Today_News;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -20,14 +23,16 @@ public class MainApp {
 ////				+ "&date="
 //				+ "&page="+1000000;
 		
-		String URL = "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=103&oid=014&aid=0004597313";
+		String URL = "https://news.naver.com/main/list.nhn?mode=LS2D&sid2=252&sid1=102&mid=shm&date=20210311&page=8";
 		
 		System.out.println("URL :: " + URL);
 		
 		URL_Crawler url_crawler = new URL_Crawler();
 		
+		Mk_File();
+		
 //		for(int i=0;i<sid1.length;i++) {
-			System.out.println(/*sid1[i]*/102);
+//			System.out.println(/*sid1[i]*/102);
 			url_crawler.select_sid2Num(/*sid1[i]*/102);
 //		}
 		
@@ -41,9 +46,10 @@ public class MainApp {
 //		Elements elements_article_text = doc.select("div.content div._article_body_contents");
 //		Elements elements_article_title = doc.select("div.content div.article_header div.article_info h3");
 //		Elements elements_article_time = doc.select("div.content div.article_header div.article_info div.sponsor span.t11");
+//		Elements elements_upload_time = doc.select("div.content div.list_body span.is_new");
 //		
 //		//3. 목록(배열)에서 정보를 가져온다.
-////		String delet_tag = "[<].*[>]";
+//		String delet_tag = "[<].*[>]";
 ////		for(Element element : elements_article_text) {
 ////			System.out.println(element.toString().replaceAll(delet_tag, "").replace("function _flash_removeCallback() {}", "").replace("// flash 오류를 우회하기 위한 함수 추가", ""));
 ////		}
@@ -54,7 +60,34 @@ public class MainApp {
 //		
 //		
 //		System.out.println(elements_nextPage.toString().split("<strong>")[1].split("</strong>")[0]);
+//		for(Element element: elements_test) {
+			
+//			if(element.toString().split("<span class=\"date")[1].split(">")[1].split("전")[0].contains("시간")) {
+//				if(7>Integer.parseInt(element.toString().split("<span class=\"date")[1].split(">")[1].split("전")[0].split("시간")[0])) {
+//					System.out.println("OK");
+//				}
+//			}
+//			if(element.toString().split("<span class=\"date")[1].split(">")[1].contains("분")) {
+//				System.out.println("OK");
+//			}
+//			else if(element.toString().split("<span class=\"date")[1].split(">")[1].contains("시간")) {
+//				System.out.println(element.toString().split("<span class=\"date")[1].split(">")[1].split("시간")[0]);
+//				if(7>Integer.parseInt(element.toString().split("<span class=\"date")[1].split(">")[1].split("시간")[0])) {
+//					System.out.println("OK");
+//				}
+//			}
+//			else {System.out.println("Fail");}
+//			
+//		}
+	}
+	
+	public static void Mk_File() throws IOException{
+
+		File Article_Data_File = new File("Article_Data.txt");
 		
+		BufferedWriter filewriter = new BufferedWriter(new FileWriter(Article_Data_File));
+		
+		filewriter.close();
 	}
 	
 }
--- src/Today_News/URL_Crawler.java
+++ src/Today_News/URL_Crawler.java
@@ -29,9 +29,10 @@ public class URL_Crawler{
 		
 		Article_Crawler article_crawler = new Article_Crawler();
 		for(int sid2_idx = 0; /*sid2_idx < sid2[sid1%100].length*/sid2_idx <1; sid2_idx++) {
-			System.out.println(/*sid2[sid1%100][sid2_idx]*/"252");
+//			System.out.println(/*sid2[sid1%100][sid2_idx]*/"252");
 			String final_page_num = final_page(sid1,/*sid2[sid1%100][sid2_idx]*/"252");
-			for(int page_num = Integer.parseInt(final_page_num); page_num > 0; page_num--) { //모든 페이지 link 가져오기
+			
+			Page_loop: for(int page_num = 1; page_num < Integer.parseInt(final_page_num); page_num++) { //모든 페이지 link 가져오기
 				
 				String URL = "https://news.naver.com/main/list.nhn?mode=LS2D&mid=shm&"
 						+ "sid2=" + /*sid2[sid1%100][sid2_idx]*/"252"
@@ -41,12 +42,11 @@ public class URL_Crawler{
 				
 				Document doc = Jsoup.connect(URL).get();
 				
-				Elements elements_test = doc.select("div.content div.list_body ul li dl"); //Aticle URL
+				Elements Article_URL = doc.select("div.content div.list_body ul li dl"); //Aticle URL
 				
-				for(Element element : elements_test) {
-					
+				for(Element element : Article_URL) {
+					if(Check_Upload_Time(element.toString().split("<span class=\"date")[1].split(">")[1])) {break Page_loop; }
 					ArrayList<String> Article_Data = article_crawler.article_crawling(element.toString().split("href=\"")[1].split("\">")[0].replace("&amp;","&")); //해당 링크에서 crawling
-					Article_Data.add(URL);
 					Article_Data.add(/*sid2[sid1%100][sid2_idx]*/"252");
 					
 					Save_File(Article_Data);
@@ -83,5 +83,21 @@ public class URL_Crawler{
 		filewriter.flush();
 		filewriter.close();
 		
+	}
+	
+	public boolean Check_Upload_Time(String Upload_Time) { //6시간전 기사 확인
+		
+		if(Upload_Time.contains("분")) {
+//			System.out.println("OK");
+			return false;
+		}
+		else if(Upload_Time.contains("시간")) {
+			if(7>Integer.parseInt(Upload_Time.split("시간")[0])) {
+//				System.out.println("OK");
+				return false;
+			}
+		}
+//		System.out.println("NO");
+		return true;
 	}
 }
